5ed467fcfc916de00b6a91b246196c26a592c81a
chaitrali-shinde <chai.shinde100@gmail.com>
2020-06-21 19:33:37 +0530

    database connencted to registration pg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
 
     private
         def user_params
-            params.require(:user).permit(:campany_id, :email, :password, :password_confirmation)
+            params.require(:user).permit(:company_id, :email, :password, :password_confirmation)
         end
 
 end
